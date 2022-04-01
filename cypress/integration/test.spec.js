7debd72e53c047bae63c6f94f5a1b5d0b0ee8d92
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-04-01 10:22:29 +0200

    clean repo after testing cypress tools

--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,14 +1,14 @@
-describe("testTest", function () {
-  it("l'alerte doit s'afficher après le click", function () {
-    const stub = cy.stub();
+// describe("testTest", function () {
+//   it("l'alerte doit s'afficher après le click", function () {
+//     const stub = cy.stub();
 
-    cy.on("window:alert", stub);
+//     cy.on("window:alert", stub);
 
-    cy.visit("http://localhost:3000/");
-    cy.get("[data-cy=submit]")
-      .click()
-      .then(() => {
-        expect(stub.getCall(0)).to.be.calledWith("click");
-      });
-  });
-});
+//     cy.visit("http://localhost:3000/");
+//     cy.get("[data-cy=submit]")
+//       .click()
+//       .then(() => {
+//         expect(stub.getCall(0)).to.be.calledWith("click");
+//       });
+//   });
+// });
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,36 +5,11 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
 
-  const [name, setName]=useState('')
-  const [mail, setMail]=useState('')
- const [result, setresult]=useState('')
 
-  const alerteTest = ()=>{
-    setresult('')
-  }
-
-  const handleSubmit = (e : any)=>{
-    e.preventDefault()
-    setresult(`${name}-${mail}`)
-    setMail('')
-    setName('')
-    
-  
-  }
-  
 
   return (
     <div className={styles.container}>
-      <button onClick={alerteTest} data-cy="submit">Click</button>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">name</label>
-        <input type="text" id='name' value={name} onChange={(e)=>setName(e.target.value)}/>
-        <label htmlFor="mail">Email</label>
-        <input type="email" id="mail" value={mail} onChange={(e)=>setMail(e.target.value)} />
-        <button type='submit' id='send'>ENVOYER</button>
-      </form>
-      {!result ? <span></span> : <span id="resultSpan">result : {result}</span>}
-  
+      TEST
      
     </div>
   )
