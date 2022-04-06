de1f606299924bcccd06ed0e96779cbfb9746655
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-04-06 22:23:08 +0200

    write test for logIn

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,7 +1,25 @@
-const { idText, createYield } = require("typescript");
-
 describe("login", function () {
-  idText("se logger dans un formulaire simple", function () {
-    cy.visit("http://localhost:3000/login");
+  it("find the login on the home page", function () {
+    cy.visit("/");
+    cy.contains("Se connecter").click();
+    cy.url().should("include", "/login");
+    cy.contains("s'inscrire");
+  });
+
+  it("not goods login informations", function () {
+    cy.visit("/login");
+    cy.get("[data-cy='email']").type("mail@mail.com");
+    cy.get('[data-cy="password"]').type("azertyuiop");
+    cy.get('[data-cy="formLogin"]').submit();
+    cy.get('[data-cy="errorMessage"]').should("not.exist");
+    cy.contains("identifiants inexistants - veuillez créer un compte");
+  });
+
+  it("login with goods informations", function () {
+    cy.visit("/login");
+    cy.get("[data-cy='email']").type("mail@mail.com");
+    cy.get('[data-cy="password"]').type("azertyuiop");
+    cy.get('[data-cy="formLogin"]').submit();
+    cy.url().should("include", "/dashboard/#user");
   });
 });
