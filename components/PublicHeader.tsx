8057360cf7dad53d0f64478f26fbafb31dbd4810
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-04-14 22:41:22 +0200

    fix pbm of the bg image on build

--- components/PublicHeader.tsx
+++ components/PublicHeader.tsx
@@ -1,5 +1,6 @@
 import style from "./styleComponents/PublicHeader.module.css";
 import Link from "next/link";
+import head from "../public/img/head.jpg";
 
 type titleProps = { title: string; link: string };
 
@@ -7,7 +8,15 @@ const PublicHeader = ({ title, link }: titleProps) => {
   return (
     <>
       <Link passHref href={link}>
-        <div className={style.headContainer}>
+        <div
+          className={style.headContainer}
+          style={{
+            backgroundImage: `url(${head.src})`,
+            height: "235px",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+          }}
+        >
           <h1 className={style.headTitle}>{title}</h1>
         </div>
       </Link>
