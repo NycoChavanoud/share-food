cb0e9a419d1edf643ddd0a869a588837aff5884b
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-04-14 10:22:10 +0200

    logger adjust for build app

--- middlewares/logger.ts
+++ middlewares/logger.ts
@@ -1,9 +1,12 @@
-import morgan from 'morgan';
+import morgan from "morgan";
+import type { NextApiRequest } from "next";
 
-morgan.token('reqbody', (req) => JSON.stringify(req.body, null, 2));
+morgan.token("reqbody", (req: NextApiRequest) =>
+  JSON.stringify(req.body, null, 2)
+);
 
 const logger = morgan(
-  ':method :url \nreq.body: :reqbody \n:status - :response-time ms\n'
+  ":method :url \nreq.body: :reqbody \n:status - :response-time ms\n"
 );
 
 export default logger;
