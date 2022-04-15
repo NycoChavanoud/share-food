4390d8237248ce39f4012e47df2ca79a08ee8439
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-04-16 00:12:08 +0200

    fix media queries for the home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import style from "../styles/Home.module.css";
 import HomeCard from "../components/HomeCard";
 import Layout from "../components/Layout";
 import PublicHeader from "../components/PublicHeader";
@@ -18,46 +19,42 @@ const Home: NextPage = (props) => {
     <>
       <Layout pageTitle="Accueil">
         <PublicHeader title="Cook of the day" link="/" />
-
-        <HomeCard
-          title="C’est vous le chef..."
-          link={srcFood}
-          content="Partagez avec vos proches vos meilleures recettes. Liberez votre
+        <div className={style.HomeCardContainer}>
+          <HomeCard
+            title="C’est vous le chef..."
+            link={srcFood}
+            content="Partagez avec vos proches vos meilleures recettes. Liberez votre
           créativité culinaire et faites en profiter l'ensemble de votre
           comunaute."
-          secondContent=" Developpez un véritable cahier de recette, pour que vos créations ne
+            secondContent=" Developpez un véritable cahier de recette, pour que vos créations ne
           soient jamais oubliées."
-        />
-
-        <HomeCard
-          title="Créer un groupe déj..."
-          link={srcCrew}
-          content="Partagez avec vos proches vos meilleures recettes. Liberez votre
+          />
+          <HomeCard
+            title="Créer un groupe déj..."
+            link={srcCrew}
+            content="Organisez vos pauses déjeuner avec vos collègues, mais bien plus encore. "
+            secondContent=" Developpez un véritable cahier de recette, pour que vos créations ne
+          soient jamais oubliées."
+          />
+          <HomeCard
+            title="Créer un groupe déj..."
+            link={srcCalendar}
+            content="Partagez avec vos proches vos meilleures recettes. Liberez votre
           créativité culinaire et faites en profiter l'ensemble de votre
           comunaute."
-          secondContent=" Developpez un véritable cahier de recette, pour que vos créations ne
+            secondContent=" Developpez un véritable cahier de recette, pour que vos créations ne
           soient jamais oubliées."
-        />
-
-        <HomeCard
-          title="Créer un groupe déj..."
-          link={srcCalendar}
-          content="Partagez avec vos proches vos meilleures recettes. Liberez votre
+          />
+          <HomeCard
+            title="Créer un groupe déj..."
+            link={srcMoney}
+            content="Partagez avec vos proches vos meilleures recettes. Liberez votre
           créativité culinaire et faites en profiter l'ensemble de votre
           comunaute."
-          secondContent=" Developpez un véritable cahier de recette, pour que vos créations ne
+            secondContent=" Developpez un véritable cahier de recette, pour que vos créations ne
           soient jamais oubliées."
-        />
-
-        <HomeCard
-          title="Créer un groupe déj..."
-          link={srcMoney}
-          content="Partagez avec vos proches vos meilleures recettes. Liberez votre
-          créativité culinaire et faites en profiter l'ensemble de votre
-          comunaute."
-          secondContent=" Developpez un véritable cahier de recette, pour que vos créations ne
-          soient jamais oubliées."
-        />
+          />
+        </div>
 
         <RegisterBtn content="Se connecter " link="/login" />
       </Layout>
