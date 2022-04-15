b045285ba8c762bf722f1085f451552098648d01
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-04-15 16:33:26 +0200

    initialize branch  home

--- components/PublicMain.tsx
+++ components/PublicMain.tsx
@@ -1,53 +1,7 @@
 import style from "./styleComponents/PublicMain.module.css";
 
 const PublicMain = () => {
-  return (
-    <div className={style.test}>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit at
-      enim. Cupiditate commodi dolor placeat ullam necessitatibus, minus vitae
-      exercitationem ducimus aut amet? Aliquid modi temporibus vero et beatae
-      dolorem dolores, fugit fuga tempore obcaecati, quam dicta nesciunt
-      praesentium mollitia impedit laboriosam facilis reprehenderit. Id
-      recusandae consequatur perferendis commodi eligendi beatae delectus in
-      repellendus aliquid deleniti quia impedit unde voluptas pariatur ad,
-      aliquam rerum tempore temporibus totam laboriosam! Praesentium totam
-      temporibus aspernatur fugit. Beatae cumque dicta omnis neque soluta
-      quibusdam, dolores doloribus ex nemo dolore illo suscipit consectetur
-      architecto iusto vitae minus ratione corrupti rerum expedita placeat ipsum
-      facere impedit. Itaque nostrum eveniet illum ab ducimus recusandae,
-      numquam soluta delectus doloremque ea excepturi ipsam aperiam est rem modi
-      harum? Odio corporis ratione sint unde doloribus vero doloremque
-      recusandae dolores similique provident aspernatur aut, eos enim ullam
-      laborum facere iusto. Architecto, soluta sapiente, officia fugiat ullam
-      pariatur commodi quasi eveniet maiores accusamus neque quia beatae vel
-      quae veritatis eaque vitae, nisi autem eos debitis dolore in delectus
-      exercitationem? Quam rerum impedit illo itaque quis nulla laborum cumque
-      nesciunt ab, illum modi nihil voluptatibus eveniet sed, repellat
-      laudantium? Necessitatibus pariatur vel dolor veritatis, laborum aperiam
-      commodi ea excepturi voluptates obcaecati labore, non minima, vitae
-      repellat dignissimos? Debitis rem eaque totam sint quo nihil voluptatum
-      maxime dicta quam, placeat illo minima at qui mollitia obcaecati
-      perferendis ipsum ad eum libero, nobis iusto quasi accusantium? Facere
-      nostrum quod sit unde iste deserunt. Quia alias voluptate provident
-      architecto modi obcaecati totam. Odio vero, tenetur odit quod quos
-      voluptatum saepe accusantium architecto id voluptates, inventore tempore
-      optio perspiciatis, quas a cumque incidunt veritatis! Sint nulla dolore,
-      saepe rerum qui soluta voluptas voluptate earum id autem, modi quibusdam?
-      Dolore sequi est ab totam necessitatibus nobis sit, hic animi cum.
-      Molestias, sapiente eligendi magni alias in repudiandae temporibus,
-      voluptate libero veritatis illo fugit rerum nisi iusto fugiat ab magnam
-      ducimus nobis corrupti sint deleniti ex distinctio, architecto nesciunt?
-      Architecto officia est voluptas quisquam numquam delectus, quo dolorem
-      impedit quasi fugit! At reiciendis nostrum pariatur impedit maiores
-      architecto aliquid dolorem velit dolores incidunt cumque commodi
-      necessitatibus repellat itaque, tempore sed iste voluptatem totam error
-      quae quaerat vitae adipisci ab. Itaque, impedit quas dignissimos facere
-      perferendis atque sunt voluptatibus. Laudantium vero soluta ullam
-      obcaecati architecto voluptatibus illum quasi cum minus, distinctio a
-      nobis quos quia facere voluptate magni quo sapiente! Cupiditate sapiente
-      veritatis fugit vitae quisquam necessitatibus tempora incidunt.
-    </div>
-  );
+  return <div className={style.test}>JE VAIS CREER UN COMPOSANT ICI</div>;
 };
 
 export default PublicMain;
