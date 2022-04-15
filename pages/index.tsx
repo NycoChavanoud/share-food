056b969efb564be896dbc45d5748f47c43fc1240
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-04-16 01:27:07 +0200

    add scroll snap effect for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,41 +18,44 @@ const Home: NextPage = (props) => {
 
   return (
     <>
-      <Layout pageTitle="Accueil">
-        <PublicHeader title="Cook of the day" link="/" />
-        <HomeBtns />
-        <div className={style.HomeCardContainer}>
-          <HomeCard
-            title="C’est vous le chef..."
-            link={srcFood}
-            content="Partagez avec vos proches vos meilleures recettes. Liberez votre
+      <div className={style.scrollContainer}>
+        <Layout pageTitle="Accueil">
+          <PublicHeader title="Cook of the day" link="/" />
+
+          <HomeBtns />
+          <div className={style.HomeCardContainer}>
+            <HomeCard
+              title="C’est vous le chef..."
+              link={srcFood}
+              content="Partagez avec vos proches vos meilleures recettes. Liberez votre
           créativité culinaire et faites en profiter l'ensemble de votre
           comunaute."
-            secondContent=" Developpez un véritable cahier de recette, pour que vos créations ne
+              secondContent=" Developpez un véritable cahier de recette, pour que vos créations ne
           soient jamais oubliées."
-          />
-          <HomeCard
-            title="Créer un groupe déj..."
-            link={srcCrew}
-            content="Organisez vos pauses déjeuner avec vos collègues, mais bien plus encore. Un restaurant, un anniversaire ou tout autres évènements. "
-            secondContent=" Ne ratez plus une occasion pour partager un moment convivial avec vos proches."
-          />
-          <HomeCard
-            title="Plannifier vos repas..."
-            link={srcCalendar}
-            content="Save the date! Vous avez prévu un évènement ?  Inviter vos groupes ou vos amis en quelques instants avec beaucoup de simplicité."
-            secondContent=" Vous recevrez sans doute en retour des invitations pour des moments inoubliables. "
-          />
-          <HomeCard
-            title="Vos budgets patagés..."
-            link={srcMoney}
-            content="Une soirée? Un repas? Communiquez facilement avec vos proches et partagez les frais de vos évènements."
-            secondContent=" Les bons comptes font les bons amis. Mais bien sûr, il est toujours possible d'être grand seigneur. "
-          />
-        </div>
+            />
+            <HomeCard
+              title="Créer un groupe déj..."
+              link={srcCrew}
+              content="Organisez vos pauses déjeuner avec vos collègues, mais bien plus encore. Un restaurant, un anniversaire ou tout autres évènements. "
+              secondContent=" Ne ratez plus une occasion pour partager un moment convivial avec vos proches."
+            />
+            <HomeCard
+              title="Plannifier vos repas..."
+              link={srcCalendar}
+              content="Save the date! Vous avez prévu un évènement ?  Inviter vos groupes ou vos amis en quelques instants avec beaucoup de simplicité."
+              secondContent=" Vous recevrez sans doute en retour des invitations pour des moments inoubliables. "
+            />
+            <HomeCard
+              title="Vos budgets patagés..."
+              link={srcMoney}
+              content="Une soirée? Un repas? Communiquez facilement avec vos proches et partagez les frais de vos évènements."
+              secondContent=" Les bons comptes font les bons amis. Mais bien sûr, il est toujours possible d'être grand seigneur. "
+            />
+          </div>
 
-        <RegisterBtn content="Se connecter " link="/login" />
-      </Layout>
+          <RegisterBtn content="Se connecter " link="/login" />
+        </Layout>
+      </div>
     </>
   );
 };
