05a8d9299a98f4f4ce9d07c411ba4f82e968333d
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-04-18 22:09:20 +0200

    add test for add event form

--- cypress/integration/event.spec.js
+++ cypress/integration/event.spec.js
@@ -14,5 +14,17 @@ describe("event", function () {
       cy.get('[data-cy="createBtn"]').click();
       cy.url().should("include", "/event/create");
     });
+    it("post new event", function () {
+      cy.visit("/event/create");
+      cy.get('[date-cy="title"]').type("Mon evenement test");
+      cy.get('[date-cy="description"]').type(
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. A qui, ullam hic dolor tempore harum quia? Minima autem quaerat, ab nulla possimus sapiente, perferendis doloremque explicabo molestiae reprehenderit, ex officiis!"
+      );
+      cy.get('[date-cy="datePicker"]').type("e.g. 2022-05-31");
+      cy.get('[date-cy="hourPicker"]').type("e.g. 12:30");
+      cy.get('[date-cy="selectEventType"]').type("Au bureau");
+      cy.get('[date-cy="adress"]').type("Au bureau");
+      cy.get('[data-cy="formAddEvent"]').submit();
+    });
   });
 });
