09c156ad5b7632b3bd6ae56d63c8c47023f6ef5c
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-04-18 19:32:23 +0200

    fix add btn on bottom of event page

--- pages/event/index.tsx
+++ pages/event/index.tsx
@@ -4,6 +4,8 @@ import PrivateHeader from "../../components/PrivateHeader";
 import style from "../../styles/Event.module.css";
 import CurrentUserContext from "../../contexts/currentUserContext";
 import { useContext, useEffect, useState } from "react";
+import plusIcon from "../../public/icons/plus.png";
+import Image from "next/image";
 
 const Event: NextPage = (props) => {
   const [firstname, setFirstname] = useState("");
@@ -18,15 +20,56 @@ const Event: NextPage = (props) => {
   }, [currentUserProfile]);
 
   return (
-    <LayoutCurrentUser pageTitle="Vos évènements">
-      <div className={style.eventPageContainer}>
-        <PrivateHeader
-          title="Planning des évènements"
-          firstname={firstname}
-          lastname={lastname}
-        />
-      </div>
-    </LayoutCurrentUser>
+    <>
+      <LayoutCurrentUser pageTitle="Vos évènements">
+        <div className={style.eventPageContainer}>
+          <PrivateHeader
+            title="Planning des évènements"
+            firstname={firstname}
+            lastname={lastname}
+          />
+          <div className={style.eventTitle}> A venir </div>
+
+          <div className={style.eventPageInfo}>
+            {/* <div className={style.eventTitle}> A venir </div> */}
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+            <div>TEST</div>
+          </div>
+
+          <div className={style.separationLine}></div>
+        </div>
+        <div className={style.addBtnContainer}>
+          <Image src={plusIcon} width={40} height={40} alt="icon-plus" />
+          <button className={style.addBtn}>créer nouvel evenement</button>
+        </div>
+      </LayoutCurrentUser>
+    </>
   );
 };
 
