98c3e2c8a0cc7e3f15fdd1f111faee4d340f60e7
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-04-25 14:33:14 +0200

    clean pages

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,14 +1,11 @@
 import { NextPage } from "next";
 import BackBtn from "../../components/BackBtn";
 import LayoutCurrentUser from "../../components/LayoutCurrentUser";
-import Wip from "../../components/Wip";
 
 const Dashboard: NextPage = (props) => {
   return (
     <LayoutCurrentUser pageTitle="tableau de bord">
       <BackBtn />
-      <h1>Dashboard</h1>
-      <Wip />
     </LayoutCurrentUser>
   );
 };
