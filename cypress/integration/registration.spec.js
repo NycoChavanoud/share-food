c2abe83ebee11862586f4536d5988c81ba422e61
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-05-05 22:05:17 +0200

    modify title on test on the registration spec

--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -4,14 +4,14 @@ describe("register", function () {
     cy.signup({ email: "dave.lopper@gmail.com", password: "azertyuiop" });
     cy.visit("/registration");
   });
-  it("find the register since home page", function () {
+  it("can find the register page from the home page", function () {
     cy.visit("/");
     cy.contains("Se connecter").click();
     cy.contains("S’inscrire").click();
     cy.url().should("include", "/registration");
   });
 
-  it("go back to the login page if i don't want register", function () {
+  it("can go back to the login page from the register page", function () {
     cy.visit("/");
     cy.contains("Se connecter").click();
     cy.contains("S’inscrire").click();
@@ -33,7 +33,7 @@ describe("register", function () {
     cy.url().should("include", "/login");
   });
 
-  it("passwords are not sames", function () {
+  it("shows an error message when passwords are not the same", function () {
     cy.visit("/registration");
     cy.get("[data-cy='email']").type("test2@gmail.com");
     cy.get('[data-cy="password"]').type("azertyuiop");
@@ -47,7 +47,7 @@ describe("register", function () {
     cy.contains("Vos mots de passes ne sont pas identiques");
   });
 
-  it("le mail de l'utilisateur existe déja", function () {
+  it("shows an error message when user already exist", function () {
     cy.visit("/registration");
     cy.get("[data-cy='email']").type("dave.lopper@gmail.com");
     cy.get('[data-cy="password"]').type("azertyuiop");
