727bcfbc38a08b8c1bbc45217180350d6e6934f9
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-05-18 17:54:10 +0200

    change some details on the page profile id

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -20,20 +20,25 @@ import { Loading } from "../../components/Loading";
 const Profile: NextPage = () => {
   const [userProfile, setUserProfile] = useState<any>("");
   const { currentUserProfile } = useContext(CurrentUserContext);
-  const firstname = currentUserProfile?.firstname;
-  const lastname = currentUserProfile?.lastname;
-  const nickName = currentUserProfile?.nickName || " ";
-  const avatarUrl = currentUserProfile?.avatarUrl;
-  const city = currentUserProfile?.city;
-  const favoritePlate = currentUserProfile?.favoritePlate;
-  const description = currentUserProfile?.description;
-
   const router = useRouter();
+  const { id } = router.query;
+  const firstname =
+    id === "me" ? currentUserProfile?.firstname : userProfile.firstname;
+  const lastname =
+    id === "me" ? currentUserProfile?.lastname : userProfile.lastname;
+  const nickName =
+    id === "me" ? currentUserProfile?.nickName : userProfile.nickName;
+  const avatarUrl =
+    id === "me" ? currentUserProfile?.avatarUrl : userProfile.avatarUrl;
+  const city = id === "me" ? currentUserProfile?.city : userProfile.city;
+  const favoritePlate =
+    id === "me" ? currentUserProfile?.favoritePlate : userProfile.favoritePlate;
+  const description =
+    id === "me" ? currentUserProfile?.description : userProfile.description;
 
   const birthday = dayjs(userProfile.birthday)
     .locale("fr")
     .format(" DD MMMM YYYY");
-  const { id } = router.query;
 
   useEffect(() => {
     if (id === "me" || !id) {
@@ -49,95 +54,89 @@ const Profile: NextPage = () => {
     }
   }, [id]);
 
-  if (!currentUserProfile && !userProfile) {
-    return <Loading />;
-  } else {
-    return (
-      <LayoutCurrentUser pageTitle="Votre profil">
-        <div className={style.profilPageContainer}>
-          <PrivateHeader
-            firstname={firstname}
-            lastname={lastname}
-            router={() => router.push("/dashboard")}
-            title={currentUserProfile ? nickName : "profil"}
-            rightElement={
-              id === "me" && (
-                <Link href="/profile/edit/">
-                  <img
-                    src={editIcon.src}
-                    alt="edit-icon"
-                    className={style.editIcon}
-                    style={{ cursor: "pointer" }}
-                    data-cy="editLink"
-                  />
-                </Link>
-              )
-            }
-          />
-
-          <div className={style.userInfoContainer}>
-            <div className={style.imageContainer}>
-              <img
-                src={currentUserProfile?.avatarUrl ? avatarUrl : avatar.src}
-                alt="avatar"
-                className={style.avatar}
-              />
-            </div>
-            <div className={style.userInfoCard}>
-              <div className={style.titleInfoCard}>
-                <div className={style.textTitle}>A propos</div>
-              </div>
-              <div className={style.detailInfoCard}>
+  return (
+    <LayoutCurrentUser pageTitle="Votre profil">
+      <div className={style.profilPageContainer}>
+        <PrivateHeader
+          firstname={firstname}
+          lastname={lastname}
+          router={() => router.push("/dashboard")}
+          title={currentUserProfile ? nickName : "profil"}
+          rightElement={
+            id === "me" && (
+              <Link href="/profile/edit/">
                 <img
-                  src={balloon.src}
-                  alt="balloon-icon"
-                  className={style.icon}
+                  src={editIcon.src}
+                  alt="edit-icon"
+                  className={style.editIcon}
+                  style={{ cursor: "pointer" }}
+                  data-cy="editLink"
                 />
-                <span className={style.textDetail}>{birthday}</span>
-              </div>
-              <div className={style.detailInfoCard}>
-                <img src={mark.src} alt="mark-icon" className={style.icon} />
-                <span className={style.textDetail}>{city}</span>
-              </div>
-              <div className={style.detailInfoCard}>
-                <img src={bol.src} alt="bol-icon" className={style.icon} />{" "}
-                <span className={style.textDetail}>{favoritePlate}</span>
-              </div>
-            </div>
-          </div>
+              </Link>
+            )
+          }
+        />
 
-          <div className={style.descriptionContainer}>
-            <div className={style.titleDescription}>
-              <span className={style.textTitleDescription}> Qui suis-je ?</span>
-              {id === "me" && (
-                <Link href="/profile/edit/">
-                  <img
-                    src={editDarkIcon.src}
-                    alt="edit-icon"
-                    className={style.editDarkIcon}
-                    style={{ cursor: "pointer" }}
-                  />
-                </Link>
-              )}
-            </div>
-            <div className={style.contentDescription}>{description}</div>
+        <div className={style.userInfoContainer}>
+          <div className={style.imageContainer}>
+            <img
+              src={currentUserProfile?.avatarUrl ? avatarUrl : avatar.src}
+              alt="avatar"
+              className={style.avatar}
+            />
           </div>
-
-          <div className={style.shareContainer}>
-            <div className={style.titleDescription}>
-              {" "}
-              <span className={style.textTitleDescription}>
-                Mes partages...
-              </span>
+          <div className={style.userInfoCard}>
+            <div className={style.titleInfoCard}>
+              <div className={style.textTitle}>A propos</div>
             </div>
-            <div style={{ textAlign: "center", marginBottom: "80px" }}>
-              coming soon
+            <div className={style.detailInfoCard}>
+              <img
+                src={balloon.src}
+                alt="balloon-icon"
+                className={style.icon}
+              />
+              <span className={style.textDetail}>{birthday}</span>
+            </div>
+            <div className={style.detailInfoCard}>
+              <img src={mark.src} alt="mark-icon" className={style.icon} />
+              <span className={style.textDetail}>{city}</span>
+            </div>
+            <div className={style.detailInfoCard}>
+              <img src={bol.src} alt="bol-icon" className={style.icon} />{" "}
+              <span className={style.textDetail}>{favoritePlate}</span>
             </div>
           </div>
         </div>
-      </LayoutCurrentUser>
-    );
-  }
+
+        <div className={style.descriptionContainer}>
+          <div className={style.titleDescription}>
+            <span className={style.textTitleDescription}> Qui suis-je ?</span>
+            {id === "me" && (
+              <Link href="/profile/edit/">
+                <img
+                  src={editDarkIcon.src}
+                  alt="edit-icon"
+                  className={style.editDarkIcon}
+                  style={{ cursor: "pointer" }}
+                />
+              </Link>
+            )}
+          </div>
+          <div className={style.contentDescription}>{description}</div>
+        </div>
+
+        <div className={style.shareContainer}>
+          <div className={style.titleDescription}>
+            {" "}
+            <span className={style.textTitleDescription}>Mes partages...</span>
+          </div>
+          <div style={{ textAlign: "center", marginBottom: "80px" }}>
+            coming soon
+          </div>
+        </div>
+      </div>
+    </LayoutCurrentUser>
+  );
 };
 
 export default Profile;
