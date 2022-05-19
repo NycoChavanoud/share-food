4ed78fcfde8e0d04d12de70b9062bae94f13059e
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-05-19 12:13:32 +0200

    copy db from dashboard branch

---
+++ prisma/migrations/20220519101153_copy_to_dashboard/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE `User` ADD COLUMN `avatarUrl` VARCHAR(191) NULL,
+    ADD COLUMN `city` VARCHAR(191) NULL,
+    ADD COLUMN `description` TEXT NULL;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -14,8 +14,86 @@ export async function seed() {
       nickName: "DavidHasseloff",
       birthday: "1982-06-08",
       favoritePlate: "pastaBox",
+      city: "Lyon",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis facilis exercitationem laborum molestias natus reprehenderit earum vero non, neque minus at aut commodi recusandae possimus amet delectus? Beatae, tempora quae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis facilis exercitationem laborum molestias natus reprehenderit earum vero non, neque minus at aut commodi recusandae possimus amet delectus? Beatae, tempora quae!",
     },
   });
+  await db.user.create({
+    data: {
+      firstname: "Nico",
+      lastname: "Chavanoud",
+      email: "nico@gmail.com",
+      hashedPassword: await hashPassword("azertyuiop"),
+      nickName: "Creator",
+      birthday: "1982-06-08",
+      favoritePlate: "kebab",
+      city: "Lyon",
+      description:
+        "Corporis facilis exercitationem.Beatae, tempora quae! Lorem ipsum dolor sit amet consectetur adipisicing elit.  Laborum molestias natus reprehenderit earum vero non, neque minus at aut commodi recusandae possimus amet delectus? Beatae, tempora quae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis facilis exercitationem laborum molestias natus reprehenderit earum vero non, neque minus at aut commodi recusandae possimus amet delectus? ",
+    },
+  });
+
+  await db.user.create({
+    data: {
+      firstname: "Toto",
+      lastname: "Larico",
+      email: "toto@gmail.com",
+      hashedPassword: await hashPassword("azertyuiop"),
+      nickName: "Larry&Co",
+      birthday: "1998-07-12",
+      favoritePlate: "Chili",
+      city: "Paris",
+      description:
+        " Tempora quae! Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis facilis exercitationem laborum molestias natus reprehenderit earum vero non, neque minus at aut commodi recusandae possimus amet delectus? facilis exercitationem laborum molestias natus reprehenderit earum vero non, neque minus at aut commodi recusandae possimus amet delectus? Beatae, Beatae, tempora quae!",
+    },
+  });
+
+  await db.user.create({
+    data: {
+      firstname: "Jean",
+      lastname: "Bon",
+      email: "Jeannot@gmail.com",
+      hashedPassword: await hashPassword("azertyuiop"),
+      nickName: "Lardus",
+      birthday: "1994-07-22",
+      favoritePlate: "Choucroute",
+      city: "Strasbourg",
+      description:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, debitis.Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore, debitis. ",
+    },
+  });
+
+  await db.user.create({
+    data: {
+      firstname: "Paul",
+      lastname: "Pulpos",
+      email: "paul@gmail.com",
+      hashedPassword: await hashPassword("azertyuiop"),
+      nickName: "El pulpo",
+      birthday: "2001-12-20",
+      favoritePlate: "fondant choco",
+      city: "Fréjus",
+      description:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus quibusdam veritatis odit voluptates ipsa natus sequi fugit beatae, optio sint repellendus maiores at incidunt obcaecati!",
+    },
+  });
+
+  await db.user.create({
+    data: {
+      firstname: "Gautier",
+      lastname: "Juste",
+      email: "juste@gmail.com",
+      hashedPassword: await hashPassword("azertyuiop"),
+      nickName: "JG",
+      birthday: "1978-02-12",
+      favoritePlate: "raclette",
+      city: "toulouse",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore ut quod excepturi odit laboriosam!",
+    },
+  });
+
   await db.event.deleteMany();
   await db.event.create({
     data: {
