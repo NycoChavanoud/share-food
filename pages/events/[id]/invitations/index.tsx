3aeafbdf366bec7ff5eb5e1c0d1cd4cf76eb1b38
NycoChavanoud <nicolas.chavanoud@gmail.com>
2022-06-27 22:21:20 +0200

    fix details

--- pages/events/[id]/invitations/index.tsx
+++ pages/events/[id]/invitations/index.tsx
@@ -80,10 +80,6 @@ const EditInvitations: NextPage = (props) => {
         guestId: userId,
         status: "PENDING",
       })
-      .then((res) => {
-        setGuests([...guests, res.data]);
-        return res;
-      })
       .then((res) => axios.get(`/api/invitations/${res.data.id}`))
       .then((i) => setGuests([...guests, i.data]))
       .catch(console.error);
